7dede74d3d6191612da362d8b6e20dea282b5793
Digna Marie Bendaña <125633846+DignaMarie14@users.noreply.github.com>
2023-03-18 22:58:30 +0800

    Add files via upload

---
+++ dblecture_conn.php
@@ -0,0 +1,14 @@
+<?php
+// $servername = "localhost";
+// $Username = "root";
+// $password = "";
+// $dbname = "midterm";
+
+$conn = mysqli_connect('localhost', 'root','', 'midterm');
+
+// Check connection
+// if ($conn->connect_error) {
+//     die("Connection failed: " . $conn->connect_error);
+// }
+
+?>
